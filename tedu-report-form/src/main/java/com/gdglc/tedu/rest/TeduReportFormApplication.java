04454266de474065efc799a0d789cae300f17451
我不语 <2628065231@qq.com>
2019-07-18 11:40:28 +0800

    #007 添加tedu-report-form模块及配置

---
+++ tedu-report-form/src/main/java/com/gdglc/tedu/rest/TeduReportFormApplication.java
@@ -0,0 +1,13 @@
+package com.gdglc.tedu.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeduReportFormApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeduReportFormApplication.class, args);
+    }
+
+}
