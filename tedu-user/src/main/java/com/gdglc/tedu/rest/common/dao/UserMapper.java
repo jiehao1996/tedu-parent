8c6a94248b2b10b72df4a513b9a66c6de9db52d0
我不语 <2628065231@qq.com>
2019-07-19 11:35:50 +0800

    #018 创建UserMapper,暂时用这个开始

---
+++ tedu-user/src/main/java/com/gdglc/tedu/rest/common/dao/UserMapper.java
@@ -0,0 +1,7 @@
+package com.gdglc.tedu.rest.common.dao;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.gdglc.tedu.rest.common.model.UserVO;
+
+public interface UserMapper extends BaseMapper<UserVO> {
+}
